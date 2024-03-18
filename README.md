2e8f16938dce9e5b5280155ec033a42ee0642447
João Victor Gouveia Rodrigues <joao.rodrigues@polodeinovacao.ifpb.edu.br>
2024-03-18 17:21:48 -0300

    first commit, workspace setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Projeto para Disciplina LP2 - Blog IFPB
+
+Esse projeto contem o trabalho de cinco estudantes do curso técnico integrado em informática do IFPB, campus João Pessoa, e contempla a nota anual da disciplina LP2(linguagens de programação 2).
+
+## Setup Geral
+
+O projeto se trata do Front End feito em ReactJS
+
+### Framework e APIs utilizadas
+
+## Módulos
+
+### Funcionalidades
---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
+// Páginas do Projeto
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
+import Profile from "./pages/profile/Profile"
+import Blog from "./pages/blog/Blog"
+import Search from "./pages/search/Search";
+
+// Components and Hooks
+import PrivateRoute from "./data/hooks/auth/PrivateRoute";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/search" element={<Search/>}/>
+        <Route element={<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
