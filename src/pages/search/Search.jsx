353b085d03c59a5ee7cd20b14c500f60ebd67a1c
João Victor Gouveia Rodrigues <joao.rodrigues@polodeinovacao.ifpb.edu.br>
2024-03-19 23:20:32 -0300

    search form update

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,16 +1,21 @@
 import { blogs } from './db.json'
-
+import { useState, useEffect } from 'react'
 import BlogResult from '../../ui/components/BlogResult'
 import './Search.css'
 
 const Search = () => {
+  const [filter, setFilter] = useState("")
+
+
+
   return (
     <div>
-      <div className='container-xxl search-container mb-5 py-3 px-5'>
-        <form>
-          <input type="text" />
-          <button>Buscar</button>
+      <div className='container-xxl search-container mb-5 py-4 px-5 mt-5'>
+        <form className='d-flex flex-wrap gap-5 pe-5'>
+          <input type="text" className='search-input nav-bar-input' />
+          <button className='btn btn-white button-outline fs-4'>Buscar</button>
         </form>
+        <p className='fs-5 '>Buscando Resultados para: <span></span></p>
       </div>
 
       <section className='result-container container-xxl bg-white p-5 mb-5'>
