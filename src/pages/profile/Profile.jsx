2374c2c4cedddb3babe4c645e6518c29913a4f3c
Raphael-Victor-Silva-Cavalcante-Viana <raphael.cavalcante@academico.ifpb.edu.br>
2024-03-24 12:03:52 -0300

    updating profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './Profile.css'
-
 const Profile = () => {
   return (
     <section className="container-xxl ">
-      <div className=''>
-        <div className='image'>
-          
+      <div className='account'>
+        <div className='imageac'>
+          <img src='https://w7.pngwing.com/pngs/213/343/png-transparent-computer-icons-user-background-icon-cdr-monochrome-name.png'/>
         </div>
-        <div className='user-info'>
-          <h1>Raphael</h1>
+        <div className='filter-container'>
+          <h3>Ariana Grande</h3>
+          <p>Aluno / Informática III</p>
+          <br/>
+          <p>Social media list</p>
+        </div>
+        <div className='background'>
+          <h4>Sobre</h4>
+          <p>Ronaldo Gogoni é formado em Análise de Desenvolvimento de Sistemas e Tecnologia da Informação pela Fatec (Faculdade de Tecnologia de São Paulo). No Tecnoblog, fez parte do TB Responde, explicando conceitos de hardware, facilitando o uso de aplicativos e ensinando truques em jogos eletrônicos. Atento ao mundo científico, escreve artigos focados em ciência e tecnologia para o Meio Bit desde 2013.</p>
+        </div>
+        <div className='postages'>
+          <h2><b>Postagens realizadas</b></h2>
         </div>
       </div>
 
